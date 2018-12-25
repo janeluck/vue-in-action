97aec3f06cc64f4338c4cc2de97ba2d490ba55a8
janeluck <janeluck158@gmail.com>
2018-12-25 21:41:27 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Awesome msg="Awesome jane"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Awesome from './components/Awesome.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Awesome
   }
 }
 </script>

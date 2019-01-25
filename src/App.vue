f674aaacc1f8b7fcdecfc73079af547b6f80ca5e
janeluck <janeluck158@gmail.com>
2019-01-25 15:22:43 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
   <!--   <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Awesome msg="Awesome jane" v-bind:btn="{name:'button01'}"/>
-    <TodoList 
-    
+    <Sweet />
+    <TodoList
+            style="display: none"
+    :add-todo="addTodo"
     newTodo="0" v-bind:todos="[{name: 'todo0', id: 't0'},{name: 'todo1', id: 't1'}]"/>
   </div>
 </template>
@@ -13,13 +15,22 @@
 //import HelloWorld from "./components/HelloWorld.vue";
 import Awesome from "./components/Awesome.vue";
 import TodoList from "./components/TodoList.vue";
+import Sweet from "./components/Sweet.vue";
 
 export default {
   name: "app",
   components: {
    // HelloWorld,
     Awesome,
-    TodoList
+    TodoList,
+    Sweet
+  },
+  data: function () {
+    return {
+      addTodo: function () {
+
+      }
+    }
   }
 };
 </script>
---
+++ src/components/Sweet.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>sweet</p>
+        <button v-bind:disabled="isButtonDisabled">Button0</button>
+        <button v-bind:disabled="!isButtonDisabled">Button1</button>
+        <div hh="jane"></div>
+    </div>
+</template>
+
+<script>
+    window.jane = 'janeluck'
+    export default {
+        name: "Sweet",
+        data: function () {
+            return {
+                isButtonDisabled: false,
+                jane: 'xxx'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
